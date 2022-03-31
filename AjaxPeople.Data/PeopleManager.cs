82f546388f59c8f38b47330aa81fe3d4df2d5bc7
Bracha Bamberger <brachabambi@gmail.com>
2022-03-30 23:24:30 -0400

    Add project files.

---
+++ AjaxPeople.Data/PeopleManager.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AjaxPeople.Data
+{
+    public class PeopleManager
+    {
+        private string _connectionString;
+        public PeopleManager(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public List<Person> GetAllPeople()
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "SELECT * FROM People";
+            connection.Open();
+            List<Person> people = new();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                people.Add(new Person
+                {
+                    FirstName = (string)reader["FirstName"],
+                    LastName = (string)reader["LastName"],
+                    Age = (int)reader["Age"]
+                });
+            }
+            return people;
+        }
+
+        public void AddPerson (Person person)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "INSERT INTO People (FirstName, LastName, Age) " +
+                "VALUES (@firstName, @lastName, @age) " +
+                "SELECT SCOPE_IDENTITY";
+            cmd.Parameters.AddWithValue("@firstName", person.FirstName);
+            cmd.Parameters.AddWithValue("@lastName", person.LastName);
+            cmd.Parameters.AddWithValue("@age", person.Age);
+            connection.Open();
+            person.Id = (int)(decimal)cmd.ExecuteScalar();
+        }
+
+        public void Delete(int id)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "DELETE FROM People WHERE Id = @id";          
+            cmd.Parameters.AddWithValue("@id", id);
+            connection.Open();
+            var reader = cmd.ExecuteReader();
+            connection.Close();
+            
+        }
+
+        public void Edit (Person person)
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var cmd = connection.CreateCommand();
+            cmd.CommandText = "UPDATE PEOPLE SET FirstName = @firstName, LastName =@lastName, Age = @age " +
+                "WHERE Id = @id";
+            cmd.Parameters.AddWithValue("@firstName", person.FirstName);
+            cmd.Parameters.AddWithValue("@lastName", person.LastName);
+            cmd.Parameters.AddWithValue("@age", person.Age);
+            connection.Open();
+            cmd.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
---
+++ AjaxPeople.Web/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using AjaxPeople.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AjaxPeople.Data;
+
+namespace AjaxPeople.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string _connectionString = @"Data Source =.\sqlexpress;Initial Catalog=PeopleAndCars;IntegratedSecurity=true;";
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            var manager = new PeopleManager(_connectionString);
+            List<Person> people = manager.GetAllPeople();
+            return Json(people);
+        }
+        [HttpPost]
+        public IActionResult AddPerson (Person person)
+        {
+            var manager = new PeopleManager(_connectionString);
+            manager.AddPerson(person);
+            return Json(person);
+        }
+
+        [HttpPost]
+        public IActionResult EditPerson (Person person)
+        {
+            var manager = new PeopleManager(_connectionString);
+            manager.Edit(person);
+            return Json(person);
+        }
+
+        [HttpPost]
+        public IActionResult DeletePerson (int id)
+        {
+            var manager = new PeopleManager(_connectionString);
+            manager.Delete(id);
+            return Json(id);
+        }
+        
+    }
+}
